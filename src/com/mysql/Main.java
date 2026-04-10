52ebdf771f480db90069c183a192d50bb18db822
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-04-10 04:42:10 +0200

    Add files via upload
    Committing Main class.

---
+++ src/com/mysql/Main.java
@@ -0,0 +1,42 @@
+/**
+ *  Java program to demonstrate java JDBC connectivity.
+ */
+
+package com.mysql;
+
+import java.sql.*;
+
+/**
+ *  Main class.
+ */
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) throws SQLException {
+
+        // Getting users wit name Jack.
+        demoMethod("Jack");
+
+    }
+
+    // Method to connect to SQL server.
+    public static void demoMethod(String input) throws SQLException {
+        String sql = "SELECT * FROM users";
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db",
+                "root", "toor");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        // Printing the results.
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("user_name"));
+        }
+
+        // Closing resources.
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+}
